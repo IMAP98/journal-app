94dc9c63181278796b64741478d9d11c7b6e8fc8
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-07-19 12:04:49 -0600

    Navbar & Sidebar added

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 
 export const JournalPage = () => {
     return (
-        <>
-            <Typography variant="h1">Journal Page</Typography>
-        </>
+        <JournalLayout>
+            <Typography>Adipisicing nostrud do dolor magna ipsum. Ea sunt eiusmod amet reprehenderit fugiat nisi laboris aliqua sit nulla anim aliquip. Veniam qui nostrud nostrud nisi sunt sunt fugiat laboris sit. Laboris nisi non incididunt anim nostrud commodo nostrud laboris. Dolor qui est id elit est aute excepteur sit dolor ad minim reprehenderit. Eu culpa non adipisicing elit commodo esse mollit enim. Anim exercitation mollit magna mollit ullamco sit esse laborum exercitation proident ad.</Typography>
+        </JournalLayout>
     );
 }
